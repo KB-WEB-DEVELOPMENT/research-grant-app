7b1bbf71de953954f5f2d176136055ed27f228f6
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-12-15 17:01:34 +0100

    Create Department.php

---
+++ Governance/app/Enums/Department.php
@@ -0,0 +1,11 @@
+<?php
+  
+namespace App\Enums;
+  
+enum Department: string
+{
+    case Biology = 'Biology';
+    case Biochemistry = 'Biochemistry';
+    case Chemistry = 'Chemistry';
+    case Physics = 'Physics';
+}
