86025f25e95f67e32ffd0cade48507365bcf0c1b
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-12-15 17:05:56 +0100

    Create Governance.php

---
+++ Governance/app/Models/Governance.php
@@ -0,0 +1,37 @@
+<?php
+ 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Enums\Department;
+ 
+class Governance extends Model
+{
+    /**
+    * The table associated with the model.
+    *
+    * @var string
+    */
+	protected $table = 'governance';
+	
+	/**
+    * The attributes that are mass assignable.
+    *
+    * @var array<int, string>
+    */
+    protected $fillable = ['dept_name','dept_initial_budget','dept_current_budget'];
+
+	/**
+    * Write code on Method
+    *
+    * @return response()
+    */
+    protected function casts(): array
+    {
+        return [
+            'dept_name' => Department::class,
+        ];
+    }
+	
+}
